28ac508e29cab6fb5af55a3cf98c28307d3f4684
Katatje <yoinkeringyoink@gmail.com>
2020-10-10 18:07:56 +0800

    Remove

--- src/main/java/me/memeszz/aurora/module/modules/combat/AutoTrap.java
+++ src/main/java/me/memeszz/aurora/module/modules/combat/AutoTrap.java
@@ -177,11 +177,11 @@ public class AutoTrap extends Module
         }
 
 
-        // TODO: dont use static bridging in offset but calculate them on the fly
+        ä
         //  based on view direction (or relative direction of target to player)
         //  (add full/half n/e/s/w patterns to append dynamically)
 
-        // TODO: sort offsetList by optimal caging success factor ->
+        ä
         // sort them by pos y up AND start building behind target
 
         int blocksPlaced = 0;
--- src/main/java/me/memeszz/aurora/module/modules/combat/AutoWeb.java
+++ src/main/java/me/memeszz/aurora/module/modules/combat/AutoWeb.java
@@ -166,11 +166,11 @@ public class AutoWeb extends Module
             Collections.addAll(placeTargets, Offsets.BODY);
         }
 
-        // TODO: dont use static bridging in offset but calculate them on the fly
+        ä
         //  based on view direction (or relative direction of target to player)
         //  (add full/half n/e/s/w patterns to append dynamically)
 
-        // TODO: sort offsetList by optimal caging success factor ->
+        ä
         // sort them by pos y up AND start building behind target
 
         int blocksPlaced = 0;
